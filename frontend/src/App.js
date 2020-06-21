d1b7f4a6966af88ae2f433d2ab4bdf6b2d33fa46
Nick Gambino <ngambino0192@gmail.com>
2020-06-20 22:57:51 -1000

    Fetch data dynamically

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,21 +28,58 @@ const news = css`
 `;
 
 function App() {
-  const [state, setState] = useState("AAPL");
-
-  // const { c } = state;
+  const [primaryData, setPrimaryData] = useState({});
+  const [primaryTicker, setPrimaryTicker] = useState("AAPL");
+  const [formField, setForm] = useState("");
 
   useEffect(() => {
-    console.log("effect");
-    axios.get("http://localhost:6969/quote/PFE").then((resp) => {
-      setState(resp.data);
-    });
-  }, []);
+    const HTTP_OK = 200;
+    const fetchData = async () => {
+      let response = await fetch(
+        `http://localhost:6969/quote/${primaryTicker}`
+      );
+      if (response.status === HTTP_OK) {
+        let json = await response.json();
+        setPrimaryData(json);
+      } else {
+        alert(`error: ${response.status}`);
+      }
+    };
+    fetchData();
+  }, [primaryTicker]);
+
+  const handleSubmit = function (ev) {
+    ev.preventDefault();
+    setPrimaryTicker(formField);
+  };
 
   return (
     <div css={{ margin: 0, padding: 0 }}>
-      <div css={header}>AAPL</div>
+      <div css={header}>
+        {primaryData.c ? (
+          <div>
+            {primaryTicker}: {primaryData.c}
+          </div>
+        ) : (
+          <div>No Results Found</div>
+        )}
+      </div>
       <div css={graph}></div>
+      <div>
+        <form onSubmit={(ev) => handleSubmit(ev)}>
+          <label htmlFor="ticker-select">
+            <input
+              id="ticker-select"
+              name="ticker-select"
+              type="text"
+              value={formField}
+              onChange={(ev) => setForm(ev.target.value)}
+              placeholder="select ticker"
+            />
+          </label>
+          <button type="submit">submit</button>
+        </form>
+      </div>
       <div css={news}></div>
     </div>
   );
