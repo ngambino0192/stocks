7425cdf4c4dc7126c5bd35c864c433aca3a5b3a3
melsoriano <mellaniesoriano@gmail.com>
2020-08-10 17:11:13 -1000

    Style article cards

--- frontend/src/components/Newslist.js
+++ frontend/src/components/Newslist.js
@@ -1,14 +1,14 @@
 /** @jsx jsx */
 // import React from "react";
-import Slider from "react-slick";
-import { css, jsx } from "@emotion/core";
+import Slider from 'react-slick';
+import { css, jsx } from '@emotion/core';
 
 const Newslist = ({ newslist }) => {
   const settings = {
     infinite: true,
     speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
+    slidesToShow: 3,
+    slidesToScroll: 3,
     initialSlide: 1,
   };
   const news = newslist.slice(0, 20);
@@ -16,11 +16,14 @@ const Newslist = ({ newslist }) => {
     <div className="w-11/12">
       <Slider {...settings}>
         {news.map((article, index) => {
+          const { image, headline, summary } = article;
           return (
-            <div key={index}>
-              <h3>{article.headline}</h3>
-              <h4>{article.summary}</h4>
-              <img height={200} src={article.image}></img>
+            <div key={`slide_item_${index}`} className="max-w-md overflow-hidden shadow-lg">
+              <img className="w-full article-image" src={image} />
+              <div className="px-6 py-4">
+                <div className="font-bold text-base mb-2">{headline}</div>
+                <p className="text-gray-700 text-sm truncate">{summary}</p>
+              </div>
             </div>
           );
         })}
