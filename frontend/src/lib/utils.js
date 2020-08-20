710c74727418550b46f783f36dd139d40153ca0e
Nick Gambino <ngambino0192@gmail.com>
2020-08-20 12:09:16 -1000

    Tweak updateWatchlist to not allow duplicates

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -7,7 +7,7 @@ export const updateWatchlist = function (
   const HTTP_OK = 200;
   const watchLimit = 6;
   const watchlistCheck = watchlist.map((company) => {
-    return company.name;
+    return company.displaySymbol;
   });
   if (!watchlistCheck.includes(primaryTicker) && primaryTicker) {
     if (watchlist.length < watchLimit) {
