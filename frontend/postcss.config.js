6106c5c4eb3354c5b03ec2a1fb07f0b012b56d5b
Nick Gambino <ngambino0192@gmail.com>
2020-09-01 21:08:34 -1000

    Future proof tailwind upgrade

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,4 +1,7 @@
 const tailwindcss = require("tailwindcss");
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
   plugins: [tailwindcss("./tailwind.js"), require("autoprefixer")],
 };
